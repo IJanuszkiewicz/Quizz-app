a39d12890a15a6bb8f5ca7df5e531d375eb03d62
IgorJanuszkiewicz <igorjanuszkiewicz997@gmail.com>
2024-04-28 14:04:29 +0200

    Add mock users for testing

---
+++ src/__mocks__/users.ts
@@ -0,0 +1,10 @@
+import { Student } from "src/graphql/models/student";
+
+export const users: [Student] = [
+    {
+        id: 1,
+        name: 'andrzej',
+        surname: 'andrzejski',
+        display_name: 'ndriu'
+    },
+];
