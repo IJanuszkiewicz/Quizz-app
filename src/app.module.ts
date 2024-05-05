82842425598eb23042c293a099b3fd6176d079a6
IgorJanuszkiewicz <igorjanuszkiewicz997@gmail.com>
2024-05-05 19:44:24 +0200

    Add container support for app and database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,32 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { StudentResolver } from './students/studentResolver';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Student } from './graphql/models/student';
 import { StudentModule } from './students/students.module';
-import { StudentService } from './students/studentService';
+import { Student } from './graphql/models/student';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.example.env', 
+      isGlobal: true, 
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'src/schema.gql'
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'admin',
-      database: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [Student],
       synchronize: true,
     }),
-    StudentModule
+    StudentModule,
   ],
   controllers: [],
   providers: [],
